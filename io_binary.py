5ea24e5a8609789a7e5589d3f39188da2aef698d
2kpr <96332338+2kpr@users.noreply.github.com>
2022-07-30 16:35:49 -0500

    New MJBA Implementation

---
+++ io_binary.py
@@ -0,0 +1,105 @@
+import struct
+import binascii
+
+class BinaryReader:
+	def __init__(self, stream):
+		self.file = stream
+
+	def seek(self, position):
+		self.file.seek(position)
+
+	def seekBy(self, value):
+		self.file.seek(self.file.tell() + value)
+
+	def tell(self):
+		return self.file.tell()
+
+	def readHex(self, length):
+		hexdata = self.file.read(length)
+		hexstr = ''
+		for h in hexdata:
+			hexstr = hex(h)[2:].zfill(2) + hexstr
+		return hexstr
+
+	def readInt64(self):
+		return struct.unpack('q', self.file.read(8))[0]
+
+	def readUInt64(self):
+		return struct.unpack('Q', self.file.read(8))[0]
+
+	def readInt(self):
+		return struct.unpack('i', self.file.read(4))[0]
+
+	def readUInt(self):
+		return struct.unpack('I', self.file.read(4))[0]
+
+	def readUShort(self):
+		return struct.unpack('H', self.file.read(2))[0]
+
+	def readShort(self):
+		return struct.unpack('h', self.file.read(2))[0]
+
+	def readByte(self):
+		return struct.unpack('b', self.file.read(1))[0]
+
+	def readUByte(self):
+		return struct.unpack('B', self.file.read(1))[0]
+
+	def readFloat(self):
+		return struct.unpack('!f', bytes.fromhex(self.readHex(4)))[0]
+
+	def readShortVec(self, size):
+		vec = [0] * size
+		for i in range(size):
+			vec[i] = self.readShort()
+		return vec
+
+	def readShortQuantizedVec(self, size, scale, bias):
+		vec = [0] * size
+		for i in range(size):
+			vec[i] = ((self.readShort() * scale[i]) / 0x7FFF) + bias[i]
+		return vec
+
+	def readUByteQuantizedVec(self, size):
+		vec = [0] * size
+		for i in range(size):
+			vec[i] = ((self.readUByte() * float(2.0)) / float(255.0)) + float(1.0)
+		return vec
+
+	def readUByteVec(self, size):
+		vec = [0] * size
+		for i in range(size):
+			vec[i] = self.readUByte()
+		return vec
+
+	def peekHex(self, length):
+		hexdata = self.file.read(length)
+		hexstr = ''
+		for h in hexdata:
+			hexstr = hex(h)[2:].zfill(2) + hexstr
+		file.seek(file.tell() - length)
+		return hexstr
+
+	def readUShortToFloatVec(self, size):
+		vec = [0] * size
+		for i in range(size):
+			vec[i] = ((self.readUShort() * float(2.0)) / float(65535.0)) - float(1.0)
+		return vec
+
+	def readFloatVec(self, size):
+		vec = [0] * size
+		for i in range(size):
+			print(self.tell())
+			print(i+1)
+			vec[i] = self.readFloat()
+		return vec
+
+	def readUBytesToBitBoolArray(self, size):
+		bit_array = ""
+		bool_array = []
+		bytes = self.readUByteVec(size)
+		for i in range(size):
+			bit_array = bin(bytes[i])[2:].zfill(8) + bit_array		
+		for i in bit_array[::-1]:
+			bool_array.append(True if i == '1' else False)
+		return { "count" : sum(bool_array), "bones" : bool_array }
---
+++ mjba_reader.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import json
+
+class Mjba:
+	def __init__(self, br):
+		self.mjba_header = MjbaHeader(br)
+		self.variable_fps = VariableFps(br)
+		self.unknown_float_data = UnknownFloatData(br)
+		self.mrtr_bone_map = MrtrBoneMap(br)
+		self.animation = Animation(br)
+
+class MjbaHeader:
+	def __init__(self, br):
+		self.mrtr_index = br.readInt64()
+		self.atmd_index = br.readInt64()
+		self.mjba_transform_matrix = br.readFloatVec(12)
+
+class VariableFps:
+	def __init__(self, br):
+		self.header_size = br.readUInt64()
+		self.frame_count = br.readUInt()
+		self.mjba_fps = br.readUInt()
+		self.fps = br.readFloatVec(self.frame_count)
+		br.seekBy(8)
+
+class UnknownFloatData:
+	def __init__(self, br):
+		self.first_size = br.readUInt()
+		self.second_size = br.readUInt()
+		size = self.first_size * self.second_size * 12
+		self.float_data = br.readFloatVec(size)
+
+class MrtrBoneMap:
+	def __init__(self, br):
+		self.fps = br.readUInt()
+		br.seekBy(4)
+		bone_header_offset = br.tell()
+		self.mrtr_bone_count = br.readUInt()
+		self.used_bone_count = br.readUInt()
+		self.mrtr_bone_offset = br.readUInt64()
+		self.used_bone_offset = br.readUInt64()
+		self.mrtr_bone_indices = br.readShortVec(self.mrtr_bone_count)
+		br.seekBy(self.used_bone_offset - self.mrtr_bone_offset - self.mrtr_bone_count * 2)
+		self.used_bone_indices = br.readShortVec(self.used_bone_count)
+		br.seekBy(0x80 - (self.used_bone_offset + self.used_bone_count * 2) % 0x80)
+		br.seekBy(0x50) # this brings you to the start of the 'essential' animation header, these bytes are the same across all MJBAs
+
+class Animation:
+	def __init__(self, br):
+		self.duration = br.readFloat()
+		self.used_bone_count = br.readUShort()
+		br.seekBy(0xA)
+		self.frame_count_1 = br.readUInt()
+		self.fps = br.readFloat()
+		animation_data_size_offset = br.tell()
+		self.animation_data_size = br.readUInt()
+		br.seekBy(4)
+		self.frame_count_1 = br.readUInt()
+		self.static_quaternion_bone_count = br.readUShort()
+		self.static_transform_bone_count = br.readUShort()
+		self.transform_scale = br.readFloatVec(3)
+		self.has_bind_poses = br.readUByte()
+		br.seekBy(3)
+		if self.used_bone_count <= 0x40:
+			self.bones_with_static_quaternions = br.readUBytesToBitBoolArray(8)
+			self.bones_with_static_transforms = br.readUBytesToBitBoolArray(8)
+			if self.has_bind_poses:
+				self.bones_with_static_bind_poses = br.readUBytesToBitBoolArray(8)
+		if self.used_bone_count > 0x40:
+			self.bones_with_static_quaternions = br.readUBytesToBitBoolArray(16)
+			self.bones_with_static_transforms = br.readUBytesToBitBoolArray(16)
+			if self.has_bind_poses:
+				self.bones_with_static_bind_poses = br.readUBytesToBitBoolArray(16)
+				br.seekBy(8)
+		self.static_bone_quaternions = br.readUShortToFloatVec(self.static_quaternion_bone_count)
+		self.dynamic_bone_quaternions = br.readUShortToFloatVec((self.used_bone_count - self.static_quaternion_bone_count) * 4 * self.frame_count_1)
+		if self.has_bind_poses:
+			self.bone_bind_poses_quaternions = br.readUShortToFloatVec(self.bones_with_static_bind_poses["count"] * 2)
+		self.static_bone_transforms = br.readUShortToFloatVec(self.static_transform_bone_count)
+		self.dynamic_bone_transforms = br.readUShortToFloatVec((self.used_bone_count - self.static_transform_bone_count) * 4 * self.frame_count_1)
+		if self.has_bind_poses:
+			self.bone_bind_poses_transforms = br.readUShortToFloatVec(self.bones_with_static_bind_poses["count"] * 2)
+		br.seekBy(self.animation_data_size - (br.tell() - animation_data_size_offset))
+		if self.animation_data_size > 0:
+			self.world_transforms = br.readFloatVec(self.frame_count_1 * 8)
