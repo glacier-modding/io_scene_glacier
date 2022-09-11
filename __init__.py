65602308bdd2e7dacd8f78cd44e70c3aba3a1be9
David <davidstulemeijer@gmail.com>
2022-09-11 15:05:31 +0200

    Added LOD panels, refactorred all files

--- __init__.py
+++ __init__.py
@@ -28,12 +28,12 @@
 # ##### END LICENSE BLOCK #####
 
 bl_info = {
-	"name": "Glacier 2 Engine Tools",
-	"description": "Tools for the Glacier 2 Engine",
-	"version": (1, 0, 0),
-	"blender": (3, 0, 0),
-	"wiki_url": "",
-	"tracker_url": "",
+    "name": "Glacier 2 Engine Tools",
+    "description": "Tools for the Glacier 2 Engine",
+    "version": (1, 0, 0),
+    "blender": (3, 0, 0),
+    "wiki_url": "",
+    "tracker_url": "",
 }
 
 import bpy
@@ -43,13 +43,14 @@ from . import file_prim
 from . import file_vtxd
 
 from bpy.props import (StringProperty,
-					   BoolProperty,
-					   PointerProperty,
-					   )
+                       BoolProperty,
+                       BoolVectorProperty,
+                       PointerProperty,
+                       )
 from bpy.types import (Panel,
-					   Operator,
-					   PropertyGroup,
-					   )
+                       Operator,
+                       PropertyGroup,
+                       )
 
 # ------------------------------------------------------------------------
 #    Global Animation Object
@@ -57,247 +58,313 @@ from bpy.types import (Panel,
 
 glacier_engine = GlacierEngine()
 
+
 # ------------------------------------------------------------------------
 #    Scene Properties
 # ------------------------------------------------------------------------
 
+class GlacierSettings(PropertyGroup):
+
+    def show_lod_update(self, context):
+
+        mesh_obs = [o for o in bpy.context.scene.objects if o.type == 'MESH']
+        for obj in mesh_obs:
+
+            should_show = False
+            for bit in range(8):
+                if self.show_lod[bit]:
+                    if obj.to_mesh().prim_properties.lod[bit] == self.show_lod[bit]:
+                        should_show = True
+
+            obj.hide_set(not should_show)
+
+        if all(self.show_lod):
+            print("All selected")
+
+        if not any(self.show_lod):
+            print("None selected")
+
+        return None
+
+    show_lod: BoolVectorProperty(
+        name='show_lod',
+        description='Set which LOD levels should be shown',
+        default=(True, True, True, True, True, True, True, True),
+        size=8,
+        subtype='LAYER',
+        update=show_lod_update,
+        # get=None,
+        # set=None
+    )
+
+
 class MyProperties(PropertyGroup):
+    mjbaimport_path: StringProperty(
+        name="MJBA Import Path",
+        description="Path to the MJBA file to import",
+        default="",
+        maxlen=1024,
+        subtype='FILE_PATH',
+    )
 
-	mjbaimport_path: StringProperty(
-		name="MJBA Import Path",
-		description="Path to the MJBA file to import",
-		default="",
-		maxlen=1024,
-		subtype='FILE_PATH',
-	)
+    mjbaexport_path: StringProperty(
+        name="MJBA Export Path",
+        description="Choose where to save the MJBA file",
+        default="",
+        maxlen=1024,
+        subtype='FILE_PATH'
+    )
 
-	mjbaexport_path: StringProperty(
-		name = "MJBA Export Path",
-		description="Choose where to save the MJBA file",
-		default="",
-		maxlen=1024,
-		subtype='FILE_PATH'
-		)
+    mrtrimport_path: StringProperty(
+        name="MRTR Import Path",
+        description="Path to the MRTR file to import",
+        default="",
+        maxlen=1024,
+        subtype='FILE_PATH'
+    )
 
-	mrtrimport_path: StringProperty(
-		name = "MRTR Import Path",
-		description="Path to the MRTR file to import",
-		default="",
-		maxlen=1024,
-		subtype='FILE_PATH'
-		)
+    mrtrexport_path: StringProperty(
+        name="MRTR Export Path",
+        description="Choose where to save the MRTR file",
+        default="",
+        maxlen=1024,
+        subtype='FILE_PATH'
+    )
 
-	mrtrexport_path: StringProperty(
-		name = "MRTR Export Path",
-		description="Choose where to save the MRTR file",
-		default="",
-		maxlen=1024,
-		subtype='FILE_PATH'
-		)
+    mjba_hash: StringProperty(
+        name="MJBA Hash/Path",
+        description="Hash or path of the MJBA file",
+        default="",
+        maxlen=1024,
+    )
 
-	mjba_hash: StringProperty(
-		name = "MJBA Hash/Path",
-		description="Hash or path of the MJBA file",
-		default="",
-		maxlen=1024,
-	)
+    mrtr_dependencyhash: StringProperty(
+        name="MRTR Hash/Path",
+        description="Specify the hash or path to a MRTR resource",
+        default="[assembly:/geometry/characters/_export_rigs/biped~~.xml].pc_rtr",
+        maxlen=1024,
+    )
 
-	mrtr_dependencyhash: StringProperty(
-		name = "MRTR Hash/Path",
-		description="Specify the hash or path to a MRTR resource",
-		default="[assembly:/geometry/characters/_export_rigs/biped~~.xml].pc_rtr",
-		maxlen=1024,
-	)
+    atmd_dependencyhash: StringProperty(
+        name="ATMD Hash/Path",
+        description="Specify a hash or path to a ATMD resource",
+        default="",
+        maxlen=1024,
+    )
 
-	atmd_dependencyhash: StringProperty(
-		name = "ATMD Hash/Path",
-		description="Specify a hash or path to a ATMD resource",
-		default="",
-		maxlen=1024,
-	)
+    mjba_worldtransform: BoolProperty(
+        name="World Transform",
+        description="Toggle whether World Transform is applied to the animation",
+        default=False
+    )
 
-	mjba_worldtransform: BoolProperty(
-		name = "World Transform",
-		description="Toggle whether World Transform is applied to the animation",
-		default = False
-	)
+    meshpicker: PointerProperty(
+        name="Mesh Picker",
+        description="Choose a mesh",
+        type=bpy.types.Object
+    )
 
-	meshpicker: PointerProperty(
-		name = "Mesh Picker",
-		description="Choose a mesh",
-		type = bpy.types.Object
-	)
+    animationpicker: PointerProperty(
+        name="Animation Picker",
+        description="Choose an animation",
+        type=bpy.types.Action
+    )
 
-	animationpicker: PointerProperty(
-		name = "Animation Picker",
-		description="Choose an animation",
-		type = bpy.types.Action
-	)
 
 # ------------------------------------------------------------------------
 #    Operators
 # ------------------------------------------------------------------------
 
 class Glacier_ImportMJBA(Operator):
-	bl_label = "Import MJBA/MRTR"
-	bl_idname = "glacier.importmjba"
+    bl_label = "Import MJBA/MRTR"
+    bl_idname = "glacier.importmjba"
 
-	def execute(self, context):
-		scene = context.scene
-		mytool = scene.my_tool
-		if mytool.mjbaimport_path == "" or mytool.mrtrimport_path == "":
-			BlenderUI.MessageBox("You must import an MJBA and it's corresponding MRTR!", icon = "ERROR")
-			return {'FINISHED'}
-		if mytool.meshpicker == None:
-			BlenderUI.MessageBox("No object selected!", icon = "ERROR")
-			return {'FINISHED'}
-		if mytool.meshpicker.type != 'ARMATURE':
-			BlenderUI.MessageBox(mytool.meshpicker.name + " does not have an armature!", icon = "ERROR")
-			return {'FINISHED'}
-		global animation
-		glacier_engine.import_animation(mytool.meshpicker, bpy.path.abspath(mytool.mjbaimport_path), bpy.path.abspath(mytool.mrtrimport_path))
+    def execute(self, context):
+        scene = context.scene
+        mytool = scene.my_tool
+        if mytool.mjbaimport_path == "" or mytool.mrtrimport_path == "":
+            BlenderUI.MessageBox("You must import an MJBA and it's corresponding MRTR!", icon="ERROR")
+            return {'FINISHED'}
+        if mytool.meshpicker == None:
+            BlenderUI.MessageBox("No object selected!", icon="ERROR")
+            return {'FINISHED'}
+        if mytool.meshpicker.type != 'ARMATURE':
+            BlenderUI.MessageBox(mytool.meshpicker.name + " does not have an armature!", icon="ERROR")
+            return {'FINISHED'}
+        global animation
+        glacier_engine.import_animation(mytool.meshpicker, bpy.path.abspath(mytool.mjbaimport_path),
+                                        bpy.path.abspath(mytool.mrtrimport_path))
 
-		return {'FINISHED'}
+        return {'FINISHED'}
+
 
 class Glacier_ExportMJBA(Operator):
-	bl_label = "Export MJBA JSON"
-	bl_idname = "glacier.exportmjba"
+    bl_label = "Export MJBA JSON"
+    bl_idname = "glacier.exportmjba"
 
-	def execute(self, context):
-		scene = context.scene
-		mytool = scene.my_tool
-		if mytool.meshpicker == None:
-			BlenderUI.MessageBox("No object selected!", icon = "ERROR")
-			return {'FINISHED'}
-		if mytool.meshpicker.type != 'ARMATURE':
-			BlenderUI.MessageBox(mytool.meshpicker.name + " does not have an armature!", icon = "ERROR")
-			return {'FINISHED'}
-		if mytool.meshpicker.animation_data == None:
-			BlenderUI.MessageBox(mytool.meshpicker.name + " does not have any animation data!", icon = "ERROR")
-			return {'FINISHED'}
-		if mytool.meshpicker.animation_data.action == None:
-			BlenderUI.MessageBox(mytool.meshpicker.name + " does not have any animation data action!", icon = "ERROR")
-			return {'FINISHED'}
-		if mytool.meshpicker.animation_data.action.fcurves == None:
-			BlenderUI.MessageBox(mytool.meshpicker.name + " does not have any animation data action fcurves!", icon = "ERROR")
-			return {'FINISHED'}
-		global animation
-		glacier_engine.export_animation(mytool.meshpicker)
+    def execute(self, context):
+        scene = context.scene
+        mytool = scene.my_tool
+        if mytool.meshpicker == None:
+            BlenderUI.MessageBox("No object selected!", icon="ERROR")
+            return {'FINISHED'}
+        if mytool.meshpicker.type != 'ARMATURE':
+            BlenderUI.MessageBox(mytool.meshpicker.name + " does not have an armature!", icon="ERROR")
+            return {'FINISHED'}
+        if mytool.meshpicker.animation_data == None:
+            BlenderUI.MessageBox(mytool.meshpicker.name + " does not have any animation data!", icon="ERROR")
+            return {'FINISHED'}
+        if mytool.meshpicker.animation_data.action == None:
+            BlenderUI.MessageBox(mytool.meshpicker.name + " does not have any animation data action!", icon="ERROR")
+            return {'FINISHED'}
+        if mytool.meshpicker.animation_data.action.fcurves == None:
+            BlenderUI.MessageBox(mytool.meshpicker.name + " does not have any animation data action fcurves!",
+                                 icon="ERROR")
+            return {'FINISHED'}
+        global animation
+        glacier_engine.export_animation(mytool.meshpicker)
 
-		return {'FINISHED'}
+        return {'FINISHED'}
+
 
 class Glacier_ExportMRTR(Operator):
-	bl_label = "Export MRTR JSON"
-	bl_idname = "glacier.exportmrtr"
+    bl_label = "Export MRTR JSON"
+    bl_idname = "glacier.exportmrtr"
 
-	def execute(self, context):
-		scene = context.scene
-		mytool = scene.my_tool
-		global animation
-		glacier_engine.export_animation(mytool.meshpicker)
+    def execute(self, context):
+        scene = context.scene
+        mytool = scene.my_tool
+        global animation
+        glacier_engine.export_animation(mytool.meshpicker)
 
-		return {'FINISHED'}
+        return {'FINISHED'}
+
 
 # ------------------------------------------------------------------------
 #    Panels
 # ------------------------------------------------------------------------
 
 class Glacier_Import_Panel(Panel):
-	bl_label = "Glacier Engine Import"
-	bl_space_type = "PROPERTIES"
-	bl_region_type = "WINDOW"
-	bl_context = "scene"
-	bl_options = {'DEFAULT_CLOSED'}
+    bl_label = "Glacier Engine Import"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "scene"
+    bl_options = {'DEFAULT_CLOSED'}
 
-	def draw(self, context):
-		layout = self.layout
-		scene = context.scene
-		mytool = scene.my_tool
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
 
-		layout.prop(mytool, "meshpicker")
-		layout.prop(mytool, "mjbaimport_path")
-		layout.prop(mytool, "mrtrimport_path")
-		layout.operator("glacier.importmjba")
-		layout.separator()
-		layout.label(text="Note: You will first need to import a GLB model from the game.")
-		layout.label(text="Or you can use the supplied template model")
-		layout.label(text="if you are making an animation for the default character rig.")
+        layout.prop(mytool, "meshpicker")
+        layout.prop(mytool, "mjbaimport_path")
+        layout.prop(mytool, "mrtrimport_path")
+        layout.operator("glacier.importmjba")
+        layout.separator()
+        layout.label(text="Note: You will first need to import a GLB model from the game.")
+        layout.label(text="Or you can use the supplied template model")
+        layout.label(text="if you are making an animation for the default character rig.")
+
 
 class Glacier_Export_Panel(Panel):
-	bl_label = "Glacier Engine Export"
-	bl_space_type = "PROPERTIES"
-	bl_region_type = "WINDOW"
-	bl_context = "scene"
-	bl_options = {'DEFAULT_CLOSED'}
+    bl_label = "Glacier Engine Export"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "scene"
+    bl_options = {'DEFAULT_CLOSED'}
 
-	def draw(self, context):
-		layout = self.layout
-		scene = context.scene
-		mytool = scene.my_tool
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
 
-		layout.label(text="Metadata Setup:")
-		layout.prop(mytool, "mjba_hash")
-		layout.prop(mytool, "mrtr_dependencyhash")
-		layout.prop(mytool, "atmd_dependencyhash")
-		layout.label(text="Note: You only need a custom ATMD file if you have want custom audio events")
-		layout.separator()
-		layout.prop(mytool, "meshpicker")
-		layout.prop(mytool, "mjbaexport_path")
-		layout.label(text="Location to save the MJBA JSON file")
-		layout.operator("glacier.exportmjba")
-		layout.prop(mytool, "mjba_worldtransform")
-		layout.separator()
-		layout.prop(mytool, "mrtrexport_path")
-		layout.label(text="Location to save the MRTR JSON file")
-		layout.label(text="Note: You only need a custom MRTR file if you have added/removed any of the bones")
-		layout.operator("glacier.exportmrtr")
+        layout.label(text="Metadata Setup:")
+        layout.prop(mytool, "mjba_hash")
+        layout.prop(mytool, "mrtr_dependencyhash")
+        layout.prop(mytool, "atmd_dependencyhash")
+        layout.label(text="Note: You only need a custom ATMD file if you have want custom audio events")
+        layout.separator()
+        layout.prop(mytool, "meshpicker")
+        layout.prop(mytool, "mjbaexport_path")
+        layout.label(text="Location to save the MJBA JSON file")
+        layout.operator("glacier.exportmjba")
+        layout.prop(mytool, "mjba_worldtransform")
+        layout.separator()
+        layout.prop(mytool, "mrtrexport_path")
+        layout.label(text="Location to save the MRTR JSON file")
+        layout.label(text="Note: You only need a custom MRTR file if you have added/removed any of the bones")
+        layout.operator("glacier.exportmrtr")
+
+
+class Glacier_Setting_Panel(bpy.types.Panel):
+    bl_idname = 'Glacier_Setting_Panel'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Glacier'
+    bl_label = 'Settings'
+
+    def draw(self, context):
+        glacier_settings = context.scene.glacier_settings
+
+        layout = self.layout
+        layout.label(text="show LOD:")
+
+        row = layout.row(align=True)
+        for i, name in enumerate(["high", "   ", "   ", "   ", "   ", "   ", "   ", "low"]):
+            row.prop(glacier_settings, "show_lod", index=i, text=name, toggle=True)
+
 
 # ------------------------------------------------------------------------
 #    Registration
 # ------------------------------------------------------------------------
-def ShowMessageBox(message = "", title = "Message Box", icon = 'INFO'):
-
+def ShowMessageBox(message="", title="Message Box", icon='INFO'):
     def draw(self, context):
         self.layout.label(text=message)
 
-    bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
-	
+    bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
+
 
 modules = [
     file_prim,
-	file_vtxd
+    file_vtxd
 ]
 
 classes = (
-	MyProperties,
-	Glacier_ImportMJBA,
-	Glacier_ExportMJBA,
-	Glacier_ExportMRTR,
-	Glacier_Import_Panel,
-	Glacier_Export_Panel,
+    MyProperties,
+    GlacierSettings,
+    Glacier_ImportMJBA,
+    Glacier_ExportMJBA,
+    Glacier_ExportMRTR,
+    Glacier_Import_Panel,
+    Glacier_Export_Panel,
+    Glacier_Setting_Panel
 )
 
+
 def register():
-	from bpy.utils import register_class
+    from bpy.utils import register_class
 
-	for module in modules:
-		module.register()
+    for module in modules:
+        module.register()
 
-	for cls in classes:
-		register_class(cls)
+    for cls in classes:
+        register_class(cls)
 
-	bpy.types.Scene.my_tool = PointerProperty(type=MyProperties)
+    bpy.types.Scene.my_tool = PointerProperty(type=MyProperties)
+    bpy.types.Scene.glacier_settings = PointerProperty(type=GlacierSettings)
+
 
 def unregister():
-	from bpy.utils import unregister_class
+    from bpy.utils import unregister_class
 
-	for module in reversed(modules):
-		module.unregister()
+    for module in reversed(modules):
+        module.unregister()
 
-	for cls in reversed(classes):
-		unregister_class(cls)
-	del bpy.types.Scene.my_tool
+    for cls in reversed(classes):
+        unregister_class(cls)
+    del bpy.types.Scene.my_tool
+    del bpy.types.Scene.glacier_settings
 
 
 if __name__ == "__main__":
-	register()
+    register()
--- file_mrtr/format.py
+++ file_mrtr/format.py
@@ -1,178 +1,181 @@
 import os
 import sys
 
+
 class MorphemeRig:
-	def __init__(self):
-		self.blend_frame_orientation = [0, 0, 0, 1]
-		self.trajectory_bone_index = 0
-		self.character_root_bone_index = 0
-		self.hierarchy = Hierarchy()
-		self.bone_name_map = StringTable()
-		self.positions = BonePositions()
-		self.quaternions = BoneQuaternions()
+    def __init__(self):
+        self.blend_frame_orientation = [0, 0, 0, 1]
+        self.trajectory_bone_index = 0
+        self.character_root_bone_index = 0
+        self.hierarchy = Hierarchy()
+        self.bone_name_map = StringTable()
+        self.positions = BonePositions()
+        self.quaternions = BoneQuaternions()
 
-	def read(self, br):
-		self.blend_frame_orientation = br.readFloatVec(4)
-		hierarchy_offset = br.readUInt64()
-		self.trajectory_bone_index = br.readUInt()
-		self.character_root_bone_index = br.readUInt()
+    def read(self, br):
+        self.blend_frame_orientation = br.readFloatVec(4)
+        hierarchy_offset = br.readUInt64()
+        self.trajectory_bone_index = br.readUInt()
+        self.character_root_bone_index = br.readUInt()
 
-		bone_name_map_offset = br.readUInt64()
-		bone_quaternions_offset = br.readUInt64()
-		bone_positions_offset = br.readUInt64()
+        bone_name_map_offset = br.readUInt64()
+        bone_quaternions_offset = br.readUInt64()
+        bone_positions_offset = br.readUInt64()
 
-		spu_memory_requirements = br.readUInt() #is always the same as the quats offset
-		br.seekBy(0x4)
+        spu_memory_requirements = br.readUInt()  # is always the same as the quats offset
+        br.seekBy(0x4)
 
-		global_id_to_rig_id_offset = br.readUInt64() #always 0
-		global_id_to_rig_id_count = br.readUInt() #always 0
-		br.seekBy(0x4)
+        global_id_to_rig_id_offset = br.readUInt64()  # always 0
+        global_id_to_rig_id_count = br.readUInt()  # always 0
+        br.seekBy(0x4)
 
-		br.seek(hierarchy_offset)
-		self.hierarchy.read(br)
+        br.seek(hierarchy_offset)
+        self.hierarchy.read(br)
 
-		br.seek(bone_name_map_offset)
-		self.bone_name_map.read(br)
+        br.seek(bone_name_map_offset)
+        self.bone_name_map.read(br)
 
-		bone_count = len(self.hierarchy.bone_parents)
+        bone_count = len(self.hierarchy.bone_parents)
 
-		br.seek(bone_positions_offset)
-		self.positions.read(br, bone_count)
+        br.seek(bone_positions_offset)
+        self.positions.read(br, bone_count)
 
-		br.seek(bone_quaternions_offset)
-		self.quaternions.read(br, bone_count)
+        br.seek(bone_quaternions_offset)
+        self.quaternions.read(br, bone_count)
 
-	def write(self, br):
+    def write(self, br):
+        # write header as 0xCD, will be constructed later
+        br.writeUByteVec([0xCD] * 0x40)
 
-		#write header as 0xCD, will be constructed later
-		br.writeUByteVec([0xCD] * 0x40)
+        br.writeUInt64(0)  # global_id_to_rig_id_offset = 0
+        br.writeUInt(0)  # global_id_to_rig_id_count = 0
+        br.align(8, 0xCD)
 
-		br.writeUInt64(0) #global_id_to_rig_id_offset = 0
-		br.writeUInt(0) #global_id_to_rig_id_count = 0
-		br.align(8, 0xCD)
+        br.writeUByteVec([0xCD] * 0x30)
 
-		br.writeUByteVec([0xCD] * 0x30)
+        hierarchy_offset = br.tell()
+        self.hierarchy.write(br)
 
-		hierarchy_offset = br.tell()
-		self.hierarchy.write(br)
+        br.align(16, 0xCD)
 
-		br.align(16, 0xCD)
+        bone_positions_offset = br.tell()
+        self.positions.write(br)
 
-		bone_positions_offset = br.tell()
-		self.positions.write(br)
+        bone_quaternions_offset = br.tell()
+        self.quaternions.write(br)
 
-		bone_quaternions_offset = br.tell()
-		self.quaternions.write(br)
+        bone_name_map_offset = br.tell()
+        self.bone_name_map.write(br)
 
-		bone_name_map_offset = br.tell()
-		self.bone_name_map.write(br)
+        br.seek(0)
+        br.writeFloatVec(self.blend_frame_orientation)
+        br.writeUInt64(hierarchy_offset)  # hierarchy_offset
+        br.writeUInt(self.trajectory_bone_index)
+        br.writeUInt(self.character_root_bone_index)
 
-		br.seek(0)
-		br.writeFloatVec(self.blend_frame_orientation)
-		br.writeUInt64(hierarchy_offset) #hierarchy_offset
-		br.writeUInt(self.trajectory_bone_index)
-		br.writeUInt(self.character_root_bone_index)
+        br.writeUInt64(bone_name_map_offset)  # bone_name_map_offset
+        br.writeUInt64(bone_quaternions_offset)  # bone_quaternions_offset
+        br.writeUInt64(bone_positions_offset)  # bone_positions_offset
+        br.writeUInt(bone_name_map_offset)  # spu_memory_requirements = bone_quaternions_offset
 
-		br.writeUInt64(bone_name_map_offset) #bone_name_map_offset
-		br.writeUInt64(bone_quaternions_offset) #bone_quaternions_offset
-		br.writeUInt64(bone_positions_offset) #bone_positions_offset
-		br.writeUInt(bone_name_map_offset) #spu_memory_requirements = bone_quaternions_offset
 
 class Hierarchy:
-	def __init__(self):
-		self.bone_parents = []
+    def __init__(self):
+        self.bone_parents = []
 
-	def read(self, br):
-		num_entries = br.readUInt()
-		br.seekBy(0x4)
-		bone_parents_offset = br.readUInt64()
-		br.seekBy(bone_parents_offset - 0x10) #the offset is relative to the offset of the num_entries
-		self.bone_parents = br.readIntVec(num_entries)
+    def read(self, br):
+        num_entries = br.readUInt()
+        br.seekBy(0x4)
+        bone_parents_offset = br.readUInt64()
+        br.seekBy(bone_parents_offset - 0x10)  # the offset is relative to the offset of the num_entries
+        self.bone_parents = br.readIntVec(num_entries)
 
-	def write(self, br):
-		br.writeUInt(len(self.bone_parents))
-		br.align(8, 0xCD)
-		br.writeUInt64(0x10)
-		br.writeIntVec(self.bone_parents)
+    def write(self, br):
+        br.writeUInt(len(self.bone_parents))
+        br.align(8, 0xCD)
+        br.writeUInt64(0x10)
+        br.writeIntVec(self.bone_parents)
+
 
 class BonePositions:
-	def __init__(self):
-		self.bone_positions = []
+    def __init__(self):
+        self.bone_positions = []
 
-	def read(self, br, bone_count):
-		for i in range(bone_count):
-			self.bone_positions.append(br.readFloatVec(3))
-			br.seekBy(0x4)
+    def read(self, br, bone_count):
+        for i in range(bone_count):
+            self.bone_positions.append(br.readFloatVec(3))
+            br.seekBy(0x4)
 
-	def write(self, br):
-		for val in self.bone_positions:
-			br.writeFloatVec(val)
-			br.align(8, 0xCD)
+    def write(self, br):
+        for val in self.bone_positions:
+            br.writeFloatVec(val)
+            br.align(8, 0xCD)
+
 
 class BoneQuaternions:
-	def __init__(self):
-		self.bone_quaternions = []
+    def __init__(self):
+        self.bone_quaternions = []
 
-	def read(self, br, bone_count):
-		for i in range(bone_count):
-			self.bone_quaternions.append(br.readFloatVec(4))
+    def read(self, br, bone_count):
+        for i in range(bone_count):
+            self.bone_quaternions.append(br.readFloatVec(4))
 
-	def write(self, br):
-		for val in self.bone_quaternions:
-			br.writeFloatVec(val)
+    def write(self, br):
+        for val in self.bone_quaternions:
+            br.writeFloatVec(val)
+
 
 class StringTable:
-	def __init__(self):
-		self.data = []
-		self.ids = []
+    def __init__(self):
+        self.data = []
+        self.ids = []
 
-	def read(self, br):
-		base_offset = br.tell()
-		num_entries = br.readUInt()
-		data_length = br.readUInt()
-		ids_offset = br.readUInt64() + base_offset
-		offsets_offset = br.readUInt64() + base_offset
-		data_offset = br.readUInt64() + base_offset
+    def read(self, br):
+        base_offset = br.tell()
+        num_entries = br.readUInt()
+        data_length = br.readUInt()
+        ids_offset = br.readUInt64() + base_offset
+        offsets_offset = br.readUInt64() + base_offset
+        data_offset = br.readUInt64() + base_offset
 
-		br.seek(ids_offset)
-		self.ids = br.readUIntVec(num_entries)
+        br.seek(ids_offset)
+        self.ids = br.readUIntVec(num_entries)
 
-		br.seek(offsets_offset)
-		offsets = br.readUIntVec(num_entries)
+        br.seek(offsets_offset)
+        offsets = br.readUIntVec(num_entries)
 
-		br.seek(data_offset) #the intended way of reading this is with the offsets array, but this is faster
-		for i in range(num_entries):
-			self.data.append(br.readCString())
+        br.seek(data_offset)  # the intended way of reading this is with the offsets array, but this is faster
+        for i in range(num_entries):
+            self.data.append(br.readCString())
 
-	def write(self, br):
-		base_offset = br.tell()
-		br.writeUInt(len(self.data)) #num_entries
+    def write(self, br):
+        base_offset = br.tell()
+        br.writeUInt(len(self.data))  # num_entries
 
-		data_length = 0
-		for str in self.data:
-			data_length = data_length + len(str) + 1
+        data_length = 0
+        for str in self.data:
+            data_length = data_length + len(str) + 1
 
-		ids_offset = 0x20
+        ids_offset = 0x20
 
-		br.writeUInt(data_length) #data_length
-		br.writeUInt64(ids_offset) #ids_offset
-		offsets_offset = len(self.data) * 0x4
-		br.writeUInt64(offsets_offset + ids_offset)  #offsets_offset
-		br.writeUInt64(offsets_offset * 2 + ids_offset) #data_offset
+        br.writeUInt(data_length)  # data_length
+        br.writeUInt64(ids_offset)  # ids_offset
+        offsets_offset = len(self.data) * 0x4
+        br.writeUInt64(offsets_offset + ids_offset)  # offsets_offset
+        br.writeUInt64(offsets_offset * 2 + ids_offset)  # data_offset
 
-		br.writeUIntVec(self.ids)
+        br.writeUIntVec(self.ids)
 
-		offsets = [0]
-		data_length = 0
-		for str in self.data:
-			offsets.append(len(str) + data_length + 1)
-			data_length = data_length + len(str) + 1
+        offsets = [0]
+        data_length = 0
+        for str in self.data:
+            offsets.append(len(str) + data_length + 1)
+            data_length = data_length + len(str) + 1
 
-		offsets.pop()
-		br.writeUIntVec(offsets)
+        offsets.pop()
+        br.writeUIntVec(offsets)
 
+        for str in self.data:
+            br.writeCString(str)
 
-		for str in self.data:
-			br.writeCString(str)
-
-		br.align(4, 0xCD)
+        br.align(4, 0xCD)
